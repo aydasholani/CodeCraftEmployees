28b3aeaa6189c1d08c4770dd2c4371f199eb7ea3
Ayda Sholani <ayda.sholani@gmail.com>
2024-04-03 17:14:41 +0200

    database create and connect

---
+++ CodeCraft/db_conn.py
@@ -0,0 +1,29 @@
+import click
+from flask import g
+from .models import db, seed_data, user_datastore
+
+def get_db():
+    if not hasattr(g, "db"):
+        g.db = db.engine.connect()
+    return g.db
+
+# Stänger databasconnection efter varje request
+def close_db(e=None):
+    db = g.pop("db", None)
+
+    if db is not None:
+        db.close()
+        
+# Initierar databasen
+@click.command("init-db")
+def init_db():
+    db.drop_all()
+    db.create_all()
+    seed_data() 
+    db.session.commit()
+    print("Database initialization successful.")
+
+def init_app(app):
+    db.init_app(app)
+    app.teardown_appcontext(close_db)
+    app.cli.add_command(init_db)
